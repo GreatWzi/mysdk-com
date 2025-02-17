c8f12214281e051f85626cb33f8dffcadbd00a46
GreatWzi <1165997296@qq.com>
2025-02-17 15:04:15 +0800

    20250217

--- docs/request.py
+++ docs/request.py
@@ -1,5 +1,13 @@
 import requests
 
-res = requests.get("https://www.baidu.com")
-
-print(res)
+def download_image(url, save_path):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        if not save_path.parent.exists():
+            save_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(save_path, 'wb') as f:
+            f.write(response.content)
+        print(f"图片已保存至：{save_path}")
+    except requests.exceptions.RequestException as e:
+        print(f"下载图片时发生错误：{e}")
